9c9fc6758b08797faeb31c56be9b57f3ca28bf49
Nikita Samoylov <vesna.nikita@yandex.ru>
2022-05-30 09:50:17 +0300

    remove unnecessary methods, storyboard objects

--- ARKit Demo/ViewController.swift
+++ ARKit Demo/ViewController.swift
@@ -20,45 +20,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         
         let scene = SCNScene()
-        
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(boxTapped))
-        self.sceneView.addGestureRecognizer(gestureRecognizer)
-        
         sceneView.scene = scene
-    }
-    
-    @objc func boxTapped(touch: UITapGestureRecognizer) {
-        let sceneView = touch.view as! SCNView
-        let touchLocation = touch.location(in: sceneView)
-        
-        let touchResults = sceneView.hitTest(touchLocation, options: [:])
-        guard !touchResults.isEmpty, let node = touchResults.first?.node else { return }
-        let boxMaterial = SCNMaterial()
-        boxMaterial.diffuse.contents = UIColor.blue
-        boxMaterial.specular.contents = UIColor.red
-        node.geometry?.materials[0] = boxMaterial
-    }
-    
-    private func createBox(in scene: SCNScene) {
-        sceneView.autoenablesDefaultLighting = true
-        
-        let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
-        let node = SCNNode(geometry: box)
-        
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.red
-        material.specular.contents = UIColor.yellow
-        
-        node.geometry?.materials = [material]
-        node.position = SCNVector3(0.0, 0.0, -1.0)
-        node.name = "box"
-        
-        scene.rootNode.addChildNode(node)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -76,21 +42,5 @@ class ViewController: UIViewController {
         
         // Pause the view's session
         sceneView.session.pause()
-    }
-    
-    @IBAction func resetTapped(sender: UIButton) {
-        session.pause()
-        sceneView.scene.rootNode.enumerateChildNodes { node, _ in
-            if node.name == "box" {
-                node.removeFromParentNode()
-            }
-        }
-        
-        let configuration = ARWorldTrackingConfiguration()
-        session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
-    }
-    
-    @IBAction func addTapped(sender: UIButton) {
-        createBox(in: sceneView.scene)
     }
 }
